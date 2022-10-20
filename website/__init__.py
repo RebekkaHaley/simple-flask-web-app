65f09ab21dca51ffa649cacb0ac4ed57f976d7c7
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-20 16:10:23 +0200

    [bugfix] fix typo

--- website/__init__.py
+++ website/__init__.py
@@ -11,7 +11,7 @@ def create_app():
     app.config['SECRET KEY'] = 'never share in production'
 
     from .views import views
-    from .views import auth
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
