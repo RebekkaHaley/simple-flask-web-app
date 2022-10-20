dcb73ba7bf6ec6f3a520a379e6a71a086c72de67
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-20 19:25:49 +0200

    [feat] add home html template

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 """Contains the standard routes for the website, e.g., the home page.
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # NB: A 'Blueprint' has a bunch of URLs pre-defined.
 views = Blueprint('views', __name__)
@@ -10,4 +10,4 @@ views = Blueprint('views', __name__)
 # NB: This function will run whenever we go to the '/' route.
 @views.route('/')
 def home():
-    return "<h1>Home</h1>"
+    return render_template("home.html")
