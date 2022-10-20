5d8cb833f25b452fae137f7e28df4630f0342cd8
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-20 19:38:59 +0200

    [feat] test jinja ifelse statements

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", user="Jane Doe", boolean=True)
 
 
 @auth.route('/logout')
