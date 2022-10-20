fd00d576b8006db964e06481909417f0d7069cbd
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-21 00:04:15 +0200

    [feat] add flash messaging to sign up

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,14 @@
 """Contains functions related to authentication.
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", user="Jane Doe", boolean=True)
+    return render_template("login.html")
 
 
 @auth.route('/logout')
@@ -16,6 +16,23 @@ def logout():
     return "<p>Logout successful</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('first_name')
+        password_one = request.form.get('password_one')
+        password_two = request.form.get('password_two')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(first_name) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password_one != password_two:
+            flash('Passwords must be equal.', category='error')
+        elif len(password_one) < 7:
+            flash('Password must be greater than 6 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
     return render_template("sign_up.html")
