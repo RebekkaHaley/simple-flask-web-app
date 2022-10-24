1d8da9503d9950454692b1bd61cf96e775665aee
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-24 14:27:47 +0200

    [feat] add sqlalchemy db and model schemas

--- website/__init__.py
+++ website/__init__.py
@@ -2,6 +2,10 @@
 """
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
@@ -9,6 +13,8 @@ def create_app():
     """
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'never share in production'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
