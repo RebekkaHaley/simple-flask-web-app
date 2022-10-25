4c35f74b5a360597dde18c7750ea2b5e4ceeb57a
Rebekka Haley <rebekka.haley@gmail.com>
2022-10-25 14:31:23 +0200

    [feat] user can delete notes

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 """Contains the standard routes for the website, e.g., the home page.
 """
 
-from flask import Blueprint, render_template, request, flash
+import json
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 
 from . import db
@@ -25,3 +26,19 @@ def home():
             db.session.commit()
             flash('Note added!', category='success')
     return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note_request = json.loads(request.data)
+    note_id = note_request['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+        else:
+            flash('This note does not belong to you!', category='error')
+    else:
+        flash('Note does not exist!', category='error')
+    return jsonify({})
